8488a279efbecc1c2242358fefbd2e8a2c35c877
chen-siyi7 <chemicalcat@me.com>
2022-09-28 02:48:09 -0500

    Update two-sample stepwise IV code.R

--- two-sample stepwise IV code.R
+++ two-sample stepwise IV code.R
@@ -24,15 +24,14 @@ twosample_mvstepIV = function(p, R, betaZX, betaZY, se_betaZY, n1, n2, sigma2_ha
   whichIV[1] = which.min(testbic)
   BICtest = NULL
   BICtest[1] = testbic[which.min(testbic)]
-  for (j in 2:(p-2)){
+  for (j in 2:p){
     testbic = NULL
-    for (i in 1:(p-2)){
+    for (i in 1:p){
       test11 = diag(x = 0, nrow = p, ncol = p, names = T)
       diag(test11)[whichIV] = 1
       diag(test11)[i] = 1
       W1 = cbind(test11, gamma_hat)
       solve.W1 = t(W1) %*% ZTZ %*% W1
-      W1 = W1[,which(non0>0)]
       beta1 = matrix(ginv(solve.W1) %*% t(W1) %*% (ZTY), ncol = 1)
       testbic[i] = n2*log(YTY - t(beta1)%*%t(W1)%*%ZTY)+log(n2)*sum(diag(test11))
     }
@@ -53,6 +52,64 @@ twosample_mvstepIV = function(p, R, betaZX, betaZY, se_betaZY, n1, n2, sigma2_ha
   beta_se = as.numeric(sqrt(Varbeta))
   my_list = list('invalidIV' = sort(invalid.IVs), 'beta_est' = beta_est, 'beta_se' = beta_se, 'K' = length(invalid.IVs))
   return(my_list)
+}
+
+
+twosample_uvstepIV = function(p, R, betaZX, betaZY, se_betaZY, n1, n2, gamma_hat, sigma2_hat){
+  p = p
+  ZTZ = R
+  ZTY = matrix(diag(ZTZ) * betaZY, ncol = 1);
+  YTY = NULL
+  for(SNP in 1:p){
+    YTY[SNP] = (n2-1)*ZTZ[SNP, SNP]*(se_betaZY^2)[SNP] + ZTY[SNP]*betaZY[SNP]
+  }
+  YTY = median(YTY)
+  
+  testbic = NULL
+  for (i in 1:(dim(ZTZ)[2])){
+    test11 = diag(x = 0, nrow = p, ncol = p, names = T)
+    diag(test11)[i] = 1
+    W1 = cbind(test11, gamma_hat)
+    solve.W1 = t(W1) %*% ZTZ %*% W1
+    beta1 = matrix(ginv(solve.W1) %*% t(W1) %*% (ZTY), ncol = 1)
+    testbic[i] = n2*log(YTY - t(beta1)%*%t(W1)%*%ZTY)+log(n2)*sum(diag(test11))
+  }
+  
+  # one by one 
+  whichIV = NULL
+  whichIV[1] = which.min(testbic)
+  BICtest = NULL
+  BICtest[1] = testbic[which.min(testbic)]
+  for (j in 2:(dim(ZTZ)[2])){
+    testbic = NULL
+    for (i in 1:(dim(ZTZ)[2])){
+      test11 = diag(x = 0, nrow = p, ncol = p, names = T)
+      diag(test11)[whichIV] = 1
+      diag(test11)[i] = 1
+      W1 = cbind(test11, gamma_hat)
+      beta1 = matrix(ginv(solve.W1) %*% t(W1) %*% (ZTY), ncol = 1)
+      testbic[i] = n2*log(YTY - t(beta1)%*%t(W1)%*%ZTY)+log(n2)*sum(diag(test11))
+    }
+    whichIV[j] = which.min(testbic)
+    BICtest[j] = testbic[which.min(testbic)]
+    if(whichIV[j] == whichIV[j-1]) break; 
+  }
+  invalid.IVs = whichIV[!duplicated(whichIV)]
+  # results
+  test11 = diag(x = 0, nrow = p, ncol = p, names = T)
+  diag(test11)[invalid.IVs] = 1
+  W1 = cbind(test11, gamma_hat)
+  solve.W1 = t(W1) %*% ZTZ %*% W1
+  beta1 = matrix(ginv(solve.W1) %*% t(W1) %*% (ZTY), ncol = 1)
+  beta_est = as.numeric(tail(beta1, n = 1))
+  C = ginv(solve.W1)%*%(t(W1)%*%ZTZ)
+  sigma_u2 = as.numeric((YTY - t(beta1)%*%t(W1)%*%ZTY))
+  Varbeta = ginv(solve.W1*n2)*sigma_u2+beta_est^2*sigma2_hat*ginv(solve.W1*n2)*n2/n1
+  Varbeta = tail(diag(Varbeta), n = 1)
+  beta_se = as.numeric(sqrt(Varbeta))
+  my_list = list('invalidIV' = sort(invalid.IVs), 'beta_est' = beta_est, 'beta_se' = beta_se, 'K' = length(invalid.IVs))
+  return(my_list)
+  
 }
 
 
